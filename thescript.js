afa63a6ab8fccaf095d731b778418ee8984c3042
Adegboyega1 <adebayogboye@gmail.com>
2021-01-26 15:29:50 +0100

    first commit

---
+++ thescript.js
@@ -0,0 +1,24 @@
+
+      /*
+      
+        Google for pictures
+
+        You are creating a search engine for pictures.
+        The user should be able to access your webpage and seach for some pictures.
+        
+        - When the user clicks on a picture, remove it from the page
+        - When a new search is done, the previous list should be removed
+
+        [HINT]
+        - use fetch to retrieve the pictures
+        - create at runtime the elements on your container
+        - use bootstrap column system to create a responsive app
+        - check the response from the API! It's not giving you back and array, but an object containing an array named "images"!
+
+        API Docs: http://www.splashbase.co/api
+        Example: http://www.splashbase.co/api/v1/images/search?query=laptop
+
+        You should change the value after "query="
+
+      */
+    
